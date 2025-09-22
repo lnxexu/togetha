29f14111ec537f699f69e3bc8462a5aef97c9733
orent143 <orentgultiano11@gmail.com>
2025-09-22 19:11:35 +1200

    WIP: local changes to requirements and server settings

--- Backend/server/decorators.py
+++ Backend/server/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import api_view, authentication_classes, permission_classes, parser_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.response import Response
