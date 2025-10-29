edcd139b9684546be0ce55bd810b8fd71871671e
Paul <paulbantawig@gmail.com>
2025-10-30 04:11:47 +0800

    fix(settings): hardcode EMAIL_HOST_USER and EMAIL_HOST_PASSWORD in settings.py

--- Backend/server/settings.py
+++ Backend/server/settings.py
@@ -36,9 +36,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True  
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_TIMEOUT = 60 
+EMAIL_HOST_USER = 'wlage35@gmail.com'
+EMAIL_HOST_PASSWORD = 'lbqi deda bbux ebxd'
+EMAIL_TIMEOUT = 60
 EMAIL_USE_LOCALTIME = False
  
 from decouple import Csv
